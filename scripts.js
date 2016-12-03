43df15480d0941a55a449bf33fcb2cfbddfe478f
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-12-02 18:21:13 -0600

    mostly done

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 console.log('JS is sourced');
 
 $(document).ready(function(){
-  var o = 'O';
-  var x = 'X';
+  var o = 'o';
+  var x = 'x';
   var whoseTurn = 0;
   //slot variables
   var slot1 = $('#slot1');
@@ -16,85 +16,87 @@ $(document).ready(function(){
   var slot9 = $('#slot9');
 
   $('#gameBoard li').on('click', function(){
-    if(slot1.hasClass('O') && slot2.hasClass('O') && slot3.hasClass('O') ||
-       slot4.hasClass('O') && slot5.hasClass('O') && slot6.hasClass('O') ||
-       slot7.hasClass('O') && slot8.hasClass('O') && slot9.hasClass('O') ||
-       slot1.hasClass('O') && slot4.hasClass('O') && slot7.hasClass('O') ||
-       slot2.hasClass('O') && slot5.hasClass('O') && slot8.hasClass('O') ||
-       slot3.hasClass('O') && slot6.hasClass('O') && slot9.hasClass('O') ||
-       slot1.hasClass('O') && slot5.hasClass('O') && slot9.hasClass('O') ||
-       slot3.hasClass('O') && slot5.hasClass('O') && slot7.hasClass('O'))
+    if(slot1.hasClass('o') && slot2.hasClass('o') && slot3.hasClass('o') ||
+       slot4.hasClass('o') && slot5.hasClass('o') && slot6.hasClass('o') ||
+       slot7.hasClass('o') && slot8.hasClass('o') && slot9.hasClass('o') ||
+       slot1.hasClass('o') && slot4.hasClass('o') && slot7.hasClass('o') ||
+       slot2.hasClass('o') && slot5.hasClass('o') && slot8.hasClass('o') ||
+       slot3.hasClass('o') && slot6.hasClass('o') && slot9.hasClass('o') ||
+       slot1.hasClass('o') && slot5.hasClass('o') && slot9.hasClass('o') ||
+       slot3.hasClass('o') && slot5.hasClass('o') && slot7.hasClass('o'))
     {
       alert('Winner is O!');
       $('#gameBoard li').text(' ');
-      // $('#gameBoard li').removeClass('disable');
-      $('#gameBoard li').removeClass('O');
-      $('#gameBoard li').removeClass('X');
+      $('#gameBoard li').removeClass('disable');
+      $('#gameBoard li').removeClass('o');
+      $('#gameBoard li').removeClass('x');
 
     }else if(
-       slot1.hasClass('X') && slot2.hasClass('X') && slot3.hasClass('X') ||
-       slot4.hasClass('X') && slot5.hasClass('X') && slot6.hasClass('X') ||
-       slot7.hasClass('X') && slot8.hasClass('X') && slot9.hasClass('X') ||
-       slot1.hasClass('X') && slot4.hasClass('X') && slot7.hasClass('X') ||
-       slot2.hasClass('X') && slot5.hasClass('X') && slot8.hasClass('X') ||
-       slot3.hasClass('X') && slot6.hasClass('X') && slot9.hasClass('X') ||
-       slot1.hasClass('X') && slot5.hasClass('X') && slot9.hasClass('X') ||
-       slot3.hasClass('X') && slot5.hasClass('X') && slot7.hasClass('X'))
+       slot1.hasClass('x') && slot2.hasClass('x') && slot3.hasClass('x') ||
+       slot4.hasClass('x') && slot5.hasClass('x') && slot6.hasClass('x') ||
+       slot7.hasClass('x') && slot8.hasClass('x') && slot9.hasClass('x') ||
+       slot1.hasClass('x') && slot4.hasClass('x') && slot7.hasClass('x') ||
+       slot2.hasClass('x') && slot5.hasClass('x') && slot8.hasClass('x') ||
+       slot3.hasClass('x') && slot6.hasClass('x') && slot9.hasClass('x') ||
+       slot1.hasClass('x') && slot5.hasClass('x') && slot9.hasClass('x') ||
+       slot3.hasClass('x') && slot5.hasClass('x') && slot7.hasClass('x'))
     {
       alert('Winner is X!');
       $('#gameBoard li').text(' ');
-      // $('#gameBoard li').removeClass('disable');
-      $('#gameBoard li').removeClass('O');
-      $('#gameBoard li').removeClass('X');
-    }else if(whoseTurn == 9){
+      $('#gameBoard li').removeClass('disable');
+      $('#gameBoard li').removeClass('o');
+      $('#gameBoard li').removeClass('x');
+
+    }else if(whoseTurn === 9){
       alert("It's a tie!");
       $('#gameBoard li').text(' ');
-      // $('#gameBoard li').removeClass('disable');
-      $('#gameBoard li').removeClass('O');
-      $('#gameBoard li').removeClass('X');
+      $('#gameBoard li').removeClass('disable');
+      $('#gameBoard li').removeClass('o');
+      $('#gameBoard li').removeClass('x');
       whoseTurn = 0;
     }else if($(this).hasClass('disable')){
-      alert('This spot has already been filled, please select another.');
-    }else if (whoseTurn%2 == 0){
+      alert('This spot has already been used, please select another.');
+    }else if (whoseTurn%2 === 0){
       whoseTurn++;
       $(this).text(o);
-      $(this).addClass('disable O');
-      if(slot1.hasClass('O') && slot2.hasClass('O') && slot3.hasClass('O') ||
-         slot4.hasClass('O') && slot5.hasClass('O') && slot6.hasClass('O') ||
-         slot7.hasClass('O') && slot8.hasClass('O') && slot9.hasClass('O') ||
-         slot1.hasClass('O') && slot4.hasClass('O') && slot7.hasClass('O') ||
-         slot2.hasClass('O') && slot5.hasClass('O') && slot8.hasClass('O') ||
-         slot3.hasClass('O') && slot6.hasClass('O') && slot9.hasClass('O') ||
-         slot1.hasClass('O') && slot5.hasClass('O') && slot9.hasClass('O') ||
-         slot3.hasClass('O') && slot5.hasClass('O') && slot7.hasClass('O'))
-         {
+      $(this).addClass('disable o');
+      if(slot1.hasClass('o') && slot2.hasClass('o') && slot3.hasClass('o') ||
+         slot4.hasClass('o') && slot5.hasClass('o') && slot6.hasClass('o') ||
+         slot7.hasClass('o') && slot8.hasClass('o') && slot9.hasClass('o') ||
+         slot1.hasClass('o') && slot4.hasClass('o') && slot7.hasClass('o') ||
+         slot2.hasClass('o') && slot5.hasClass('o') && slot8.hasClass('o') ||
+         slot3.hasClass('o') && slot6.hasClass('o') && slot9.hasClass('o') ||
+         slot1.hasClass('o') && slot5.hasClass('o') && slot9.hasClass('o') ||
+         slot3.hasClass('o') && slot5.hasClass('o') && slot7.hasClass('o'))
+       {
            alert("Winner is O!");
            whoseTurn = 0;
+         }
     }else{
       whoseTurn++;
       $(this).text(x);
-      $(this).addClass('disable X');
-      if(slot1.hasClass('X') && slot2.hasClass('X') && slot3.hasClass('X') ||
-        slot4.hasClass('X') && slot5.hasClass('X') && slot6.hasClass('X') ||
-        slot7.hasClass('X') && slot8.hasClass('X') && slot9.hasClass('X') ||
-        slot1.hasClass('X') && slot4.hasClass('X') && slot7.hasClass('X') ||
-        slot2.hasClass('X') && slot5.hasClass('X') && slot8.hasClass('X') ||
-        slot3.hasClass('X') && slot6.hasClass('X') && slot9.hasClass('X') ||
-        slot1.hasClass('X') && slot5.hasClass('X') && slot9.hasClass('X') ||
-        slot3.hasClass('X') && slot5.hasClass('X') && slot7.hasClass('X'))
+      $(this).addClass('disable x');
+      if(slot1.hasClass('x') && slot2.hasClass('x') && slot3.hasClass('x') ||
+        slot4.hasClass('x') && slot5.hasClass('x') && slot6.hasClass('x') ||
+        slot7.hasClass('x') && slot8.hasClass('x') && slot9.hasClass('x') ||
+        slot1.hasClass('x') && slot4.hasClass('x') && slot7.hasClass('x') ||
+        slot2.hasClass('x') && slot5.hasClass('x') && slot8.hasClass('x') ||
+        slot3.hasClass('x') && slot6.hasClass('x') && slot9.hasClass('x') ||
+        slot1.hasClass('x') && slot5.hasClass('x') && slot9.hasClass('x') ||
+        slot3.hasClass('x') && slot5.hasClass('x') && slot7.hasClass('x'))
           {
             alert("Winner is X!");
             whoseTurn = 0;
           }
       }
-      //reset button
+      //reset button to reset the game
       $('#resetButton').click(function(){
+        console.log( 'reset button clicked' );
         $('#gameBoard li').text(' ');
-        // $('#gameBoard li').removeClass('disable');
-        $('#gameBoard li').removeClass('O');
-        $('#gameBoard li').removeClass('X');
+        $('#gameBoard li').removeClass('disable');
+        $('#gameBoard li').removeClass('o');
+        $('#gameBoard li').removeClass('x');
         whoseTurn = 0;
       });
-    }
+    });
   });
-});
